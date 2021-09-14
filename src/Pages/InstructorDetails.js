899512e63f47194bee9c037e1dfdc147a7ede687
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-09-14 14:25:58 -0400

    added style to contact form inside instructor.js

--- src/Pages/InstructorDetails.js
+++ src/Pages/InstructorDetails.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom"
 import "../styles/InstructorDetails.css";
 
-
 function InstructorDetails() {
-     const baseURL = "https://learn-pro-phase4-backend.herokuapp.com/";
-     const { id } = useParams();
-     const [instructor, setInstructor] = useState([]);
+  const baseURL = "https://learn-pro-phase4-backend.herokuapp.com/";
+  const { id } = useParams();
+  const [instructor, setInstructor] = useState([]);
 
-     //getting request by id
+  //getting request by id
   useEffect(() => {
     fetch(`${baseURL}/instructors/${id}`)
       .then((res) => res.json())
@@ -17,51 +15,71 @@ function InstructorDetails() {
     // eslint-disable-next-line
   }, []);
 
-    return (
-        <div className="ins-profile">
-            {instructor && (
+  return (
+    <div className="ins-profile">
+      {instructor && (
+        <>
+          <div className="ins-content">
+            <div className="instructor-card">
+              <h1 className="ins-heading">{instructor.name}</h1>
+            </div>
+            <div className="div-content">
+              <div className="ins-para">
+                <p>{instructor.description}</p>
+              </div>
+              <img
+                className="ins-img"
+                alt={"instructor-img"}
+                src={instructor.image}
+              />
+            </div>
+
+            <div>
+              <h2 className="contact-head">
+                Any Question? Contact directly to {instructor.name}
+              </h2>
+              <div>
                 <>
-                <div className="ins-content">
-                   <div className="instructor-card">
-                       <h1 className="ins-heading">{instructor.name}</h1>
-                   </div >
-                   <div className="div-content">
-                       <div className="ins-para"><p >{instructor.description}</p></div>
-                       <img className="ins-img" alt={"instructor-img"} src={instructor.image} />
-                   </div>
+                  <div className="container1">
+                    <p>CONTACT ME</p>
 
-                    <div>
-                  <h2 className="contact-head">Any Question? Contact directly to {instructor.name}</h2>
-                  <div>
-                      <>
-     <div className="container1">
-    <p>CONTACT ME</p>
-    
-    <div className="login">
-      <input type="text" placeholder="Your Name" className="input" />
-      <input type="text" placeholder="Your Email Address" className="input" />
-    </div>
-    
-    <div className="subject">
-      <input type="text" placeholder="Subject" className="input" />
-    </div>
-    
-    <div className="msg">
-      <textarea  className="area" placeholder="Leave a Message"></textarea>
-    </div>
-    
-    <div className="btn-2">Send Message</div>
-  </div>
+                    <div className="login">
+                      <input
+                        type="text"
+                        placeholder="Your Name"
+                        className="input"
+                      />
+                      <input
+                        type="text"
+                        placeholder="Your Email Address"
+                        className="input"
+                      />
+                    </div>
 
+                    <div className="subject">
+                      <input
+                        type="text"
+                        placeholder="Subject"
+                        className="input"
+                      />
+                    </div>
 
-</>
+                    <div className="msg">
+                      <textarea
+                        className="area"
+                        placeholder="Leave a Message"
+                      ></textarea>
+                    </div>
+                    <div className="btn-2">Send Message</div>
                   </div>
-                </div>
-                </div>
                 </>
-            )}
-        </div>
-    )
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default InstructorDetails
+export default InstructorDetails;
