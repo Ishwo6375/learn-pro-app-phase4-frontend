e73f3712baaad216c20aec1de73f42431906a530
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-09-20 14:11:47 -0400

    added email sending feature in contact form

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import "../styles/contact.css"
+import emailjs from 'emailjs-com'
 
 function Contact() {
+
+
+  function sendEmail(e){
+   e.preventDefault(); 
+   emailjs.sendForm('service_9b93s0p', 'template_x5r8lda', e.target,
+    "user_6sPNsSlwqHcFn0VD0xIvh").then(res=>{
+      console.log(res);
+    }).catch(err => console.log(err));
+  }
     return (
         <>
             <h1 className="contact-head">Contact Us</h1>
@@ -9,12 +19,12 @@ function Contact() {
       <div className="contactOverlay">
         <div className="container">
           <div className="form">
-            <form className="form-2">
+            <form className="form-2" onSubmit={sendEmail}>
               <div className="formWord">
                 <h2>Say Hello!</h2>
                 <span>Full Name</span>
                 <br />
-                <input className="input100" type="text" name="fullName" required />
+                <input className="input100" type="text" name="name" required />
                 <br />
                 <span>Phone Number</span>
                 <br />
